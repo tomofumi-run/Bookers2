4178077ff511b8f7a7e8957bed0cfa6095dcb1da
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-04-02 00:58:09 +0000

    [Add]新規投稿実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:name,:body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   
   def show
+    # ログイン時
     @user = User.find(params[:id])
+    # 新規投稿時
+    @book = Book.new
   end
 end
