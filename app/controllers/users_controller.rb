9e92b6003cce25c38c4f3700398355c962308c84
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-04-03 22:17:00 +0000

    [Add]CRUD実装OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     
+  def index
+   @users = User.all
+   @user = current_user
+  end
+  
   def show
     @user = User.find(params[:id])
     @book = Book.new
