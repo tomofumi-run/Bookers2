ae68db77ae3b528264c03a51ee9f76de6d4998e7
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-04-05 00:09:05 +0000

    [Add]ユーザー編集でのエラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
          
   attachment :profile_image
   has_many :books, dependent: :destroy
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
 end
