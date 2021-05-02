e75bdc59cce12eba5c05093d37786ce8459d81fe
tomofumi-run <nnsu_1014@yahoo.co.jp>
2021-05-02 11:32:54 +0000

    [search機能]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   root to: "homes#top"
   get "home/about", to: "homes#about"
+  get '/search', to: 'search#search'
   
   resources :books, only: [:index, :show, :edit, :update, :create, :destroy]
   resources :users, only: [:index, :show, :new, :edit, :update]
